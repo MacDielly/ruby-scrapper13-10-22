da5eb447963157a2b119d307005b1d2b049707f0
kevin <kevin@soucheyre.org>
2022-10-13 21:11:00 +0200

    second commit

--- scrapper_nokogiri/lib/mairie_christmas.rb
+++ scrapper_nokogiri/lib/mairie_christmas.rb
@@ -1,25 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 
-
-def some_mails()
-#   emails = []
-  page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/95/ableiges.html"))
-  get_mail = page do |i|
-    i = def name(link)
-    bourse = []
+def connect
+    page = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
   
-    link.xpath('/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |i|
-      bourse.push (i.text)
+    townhalls = Array.new(0)
+  
+    page.css('//a[@class=lientxt]/@href').each.with_index do |href, index| # boucle pour extraire les informations
+      complete_url = "http://annuaire-des-mairies.com/#{href.to_s[2, href.to_s.length]}"
+      townhall = Nokogiri::HTML(URI.open(complete_url))
+  
+      city = townhall.css('h1')[1].text.split(' - ')[0].to_s
+      email = townhall.css('tr.txt-primary')[3].css('td')[1].text.to_s
+  
+      townhalls[index] = { city => email } # On met les Hashs dans un Array
+  
+      puts("La mairie de #{city} a l'adresse email #{email}") # On met un petit message
     end
-    return bourse
-  end
-end
   
-  all_pages = page do |j|
-    j = page.slice(0..(page.index('95/'))) #Puis trouver un moyen d'alle sur l'url suivant
-end
-end
-
-puts some_mails()
-
+    return townhalls
+  end
+  
+  puts connect
